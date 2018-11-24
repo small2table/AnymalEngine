b0650e51aaa8b6b3c43f7d127c1f5135c325fd90
Fercury <kwon9804@kaist.ac.kr>
2018-11-24 19:54:46 +0900

    time after dinner

--- AnymalTime.cpp
+++ AnymalTime.cpp
@@ -3,7 +3,7 @@
 namespace anymal {
 
 AnymalTime::AnymalTime() {
-	this->prev = std::chrono::high_resolution_clock::now();
+	this->prev = std::chrono::system_clock::now();
 }
 
 AnymalTime::~AnymalTime() {
@@ -12,10 +12,15 @@ AnymalTime::~AnymalTime() {
 
 long long AnymalTime::tick() {
 		
-	std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
+	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
 	std::chrono::milliseconds dur = std::chrono::duration_cast<std::chrono::milliseconds>(now - this->prev) ;
 	this->prev = now;
 	return dur.count();
+}
+
+std::time_t * AnymalTime::getCurrentTime() {
+	this->currentTime = std::chrono::system_clock::to_time_t(this->prev);
+	return &(this->currentTime);
 }
 
 }
